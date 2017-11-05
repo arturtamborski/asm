369e16ff91b1fac3fd4477566e37fc040e7982ab
Artur Tamborski <tamborskiartur@gmail.com>
2017-11-05 11:32:24 +0100

    add example projects

---
+++ drop-shell/src/shell.s
@@ -0,0 +1,12 @@
+section .text
+	global _start
+
+_start:
+	mov	edx,	0	; envp
+	mov 	ecx, 	0	; argv
+	mov	ebx,	msg	; &msg
+	mov	eax,	11	; execve()
+	int	0x80
+
+section .data
+	msg	db	"/bin/sh"
---
+++ hello-world/src/hello.s
@@ -0,0 +1,19 @@
+; Just a stupid hello world program
+
+section .text
+    global _start
+
+_start:
+    mov     edx,    len     ; strlen(msg)
+    mov     ecx,    msg     ; &msg
+    mov     ebx,    1       ; stdout
+    mov     eax,    4       ; write()
+    int     0x80
+    
+    mov     ebx,    0       ; return value
+    mov     eax,    1       ; exit()
+    int     0x80
+
+section .data
+    msg     db      "Hello World!", 0xA ; line feed
+    len     equ     $ - msg
